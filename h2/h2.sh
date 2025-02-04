9c657d5e7777df659b2c940ca4b5d5aeaaece7db
Ed Solovey <ed@digits.com>
2025-02-04 10:29:55 -0500

    make h2 wait for debugger

--- h2/h2.sh
+++ h2/h2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 dir=$(dirname "$0")
-java -cp "$dir/target/h2-2.3.239-SNAPSHOT.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -cp "$dir/target/h2-2.3.239-SNAPSHOT.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"
