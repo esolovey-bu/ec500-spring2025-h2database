24273ff55f28e6dbf74e2c92302fc188656b13fb
Ed Solovey <ed@digits.com>
2025-03-09 14:45:07 -0400

    Set up mockito

--- h2/src/main/org/h2/command/query/RuleBasedJoinOrderPicker.java
+++ h2/src/main/org/h2/command/query/RuleBasedJoinOrderPicker.java
@@ -1,4 +1,18 @@
 package org.h2.command.query;
 
+import org.h2.engine.SessionLocal;
+import org.h2.table.TableFilter;
+
 public class RuleBasedJoinOrderPicker {
+  final SessionLocal session;
+  final TableFilter[] filters;
+
+  public RuleBasedJoinOrderPicker(SessionLocal session, TableFilter[] filters) {
+    this.session = session;
+    this.filters = filters;
+  }
+
+  public TableFilter[] bestOrder(){
+    return filters;
+  }
 }
--- h2/src/test/org/h2/command/query/RuleBasedJoinOrderPickerTest.java
+++ h2/src/test/org/h2/command/query/RuleBasedJoinOrderPickerTest.java
@@ -1,17 +1,35 @@
 package org.h2.command.query;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.h2.engine.SessionLocal;
+import org.h2.table.Table;
+import org.h2.table.TableFilter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 public class RuleBasedJoinOrderPickerTest {
+  SessionLocal mockSession;
+
+  RuleBasedJoinOrderPicker ruleBasedJoinOrderPicker;
+
   @BeforeEach
   public void setUp(){
-
+    mockSession = Mockito.mock(SessionLocal.class);
+    Mockito.when(mockSession.nextObjectId()).thenReturn(1);
   }
 
   @Test
-  public void first(){
-    assertEquals("hey", "hey");
+  public void bestOrder_singleTable(){
+    Table customers = Mockito.mock(Table.class);
+
+    TableFilter tableFilter = new TableFilter(mockSession, customers, "customers", true, null, 0, null);
+    TableFilter[] filters = {tableFilter};
+
+    ruleBasedJoinOrderPicker = new RuleBasedJoinOrderPicker(mockSession, filters);
+
+    assertEquals(filters, ruleBasedJoinOrderPicker.bestOrder());
   }
 }
